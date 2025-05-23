ac69165513a764abce8afeb0f7aaf25b435fe008
송석리(Song Sukree) <greatsong21@gmail.com>
2025-05-23 10:43:05 +0900

    Update 00_o3.py

--- pages/00_o3.py
+++ pages/00_o3.py
@@ -29,7 +29,7 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-CORRECT_PASSWORD = "greatsong"
+CORRECT_PASSWORD = "zzolab"
 
 # --- CSS ---------------------------------------------------------------------
 CUSTOM_CSS = """
